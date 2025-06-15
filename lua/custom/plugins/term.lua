218dbe088d605565bca3ccedad4075a11a19ed48
Doheny Yoon <dyoon@snapchat.com>
2025-06-15 14:53:58 -0700

    adding xcode nvim integration

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -1,19 +1,9 @@
 return {
-  vim.api.nvim_create_autocmd('TermOpen', {
-    group = vim.api.nvim_create_augroup('custom-term-open', {
-      clear = true,
-    }),
-    callback = function()
-      vim.opt.number = false
-      vim.opt.relativenumber = false
-    end,
-  }),
   vim.keymap.set('n', '<leader>tt', function()
     vim.cmd.new()
     vim.cmd.term()
     vim.cmd.wincmd 'J'
     vim.api.nvim_win_set_width(0, 80)
     vim.cmd 'startinsert'
-    print 'testing'
   end),
 }
