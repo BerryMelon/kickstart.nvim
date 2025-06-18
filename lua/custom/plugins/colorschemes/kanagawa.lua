da82160840c58db1871ccac56e79f87f98ab1aeb
Doheny Yoon <dyoon@snapchat.com>
2025-06-17 19:33:40 -0700

    adding colorscheme

--- lua/custom/plugins/colorschemes/kanagawa.lua
+++ lua/custom/plugins/colorschemes/kanagawa.lua
@@ -1,8 +1,9 @@
 return {
-  'rebelot/kanagawa.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'kanagawa',
+  'thesimonho/kanagawa-paper.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd 'colorscheme kanagawa-paper'
+  end,
 }
