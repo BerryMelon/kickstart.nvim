5e5ffc896e1c5433a784be6c9afe47af65d6a21d
Doheny Yoon <dyoon@snapchat.com>
2025-06-22 00:57:20 -0700

    modifying floaterm

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -1,10 +1,8 @@
 return {
   'voldikss/vim-floaterm',
   config = function()
-    vim.g.floaterm_width = 0.8
-    vim.g.floaterm_height = 0.8
     vim.keymap.set('n', '<leader>tt', '<CMD>FloatermToggle<CR>', { desc = 'Toggle Floaterm Window' })
-    vim.keymap.set('n', '<leader>tn', '<CMD>FloatermNew<CR>', { desc = 'Open New Floaterm Window' })
+    vim.keymap.set('n', '<leader>tn', '<CMD>FloatermNew --cwd=<buffer> zsh<CR>', { desc = 'Open New Floaterm Window' })
     vim.keymap.set('n', '<leader>th', '<CMD>FloatermPrev<CR>', { desc = 'Prev Floaterm Window' })
     vim.keymap.set('n', '<leader>tl', '<CMD>FloatermNext<CR>', { desc = 'Next Floaterm Window' })
     vim.keymap.set('n', '<leader>tq', '<CMD>FloatermKill<CR>', { desc = 'Kill Floaterm Window' })
